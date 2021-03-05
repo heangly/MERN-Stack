50ef8bc956879dc62382ce18419f805a71687ef2
Heang Ly <heanglypa@gmail.com>
2021-03-05 16:22:38 -0500

    implement product screen

--- proshop/frontend/src/App.js
+++ proshop/frontend/src/App.js
@@ -3,18 +3,21 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import { Container } from 'react-bootstrap';
 import HomeScreen from './screens/HomeScreen';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
-      <main className='py-3'>
+      <main className='py-5'>
         <Container>
-          <HomeScreen />
+          <Route exact path='/' component={HomeScreen} />
+          <Route path='/product/:id' component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
