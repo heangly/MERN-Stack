1cd796a2043a1e58910ce2f9071b0b90ab18bdeb
Heang Ly <heanglypa@gmail.com>
2021-03-07 23:52:03 -0500

    set up db connection

--- proshop/backend/server.js
+++ proshop/backend/server.js
@@ -2,8 +2,11 @@ import express from 'express';
 import colors from 'colors';
 import products from './data/products.js';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 
 dotenv.config();
+connectDB();
+
 const app = express();
 app.use(express.json());
 
