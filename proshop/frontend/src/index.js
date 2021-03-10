deeba8fd9ac8cea8716f62d0438f0308242d362e
Heang Ly <heanglypa@gmail.com>
2021-03-10 09:37:38 -0500

    set up redux

--- proshop/frontend/src/index.js
+++ proshop/frontend/src/index.js
@@ -1,7 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/bootstrap.min.css';
-import './styles/index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import store from './store'
+import './styles/bootstrap.min.css'
+import './styles/index.css'
+import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
