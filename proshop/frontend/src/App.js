9549535edf25d7275225a4b4b47ff6f6901fcef8
Heang Ly <heanglypa@gmail.com>
2021-03-11 20:02:51 -0500

    add CART screen

--- proshop/frontend/src/App.js
+++ proshop/frontend/src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import { Container } from 'react-bootstrap';
-import HomeScreen from './screens/HomeScreen';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import ProductScreen from './screens/ProductScreen';
+import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Container } from 'react-bootstrap'
+import HomeScreen from './screens/HomeScreen'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/CartScreen'
 
 const App = () => {
   return (
@@ -14,11 +15,12 @@ const App = () => {
         <Container>
           <Route exact path='/' component={HomeScreen} />
           <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/cart/:id?' component={CartScreen} />
         </Container>
       </main>
       <Footer />
     </Router>
-  );
-};
+  )
+}
 
-export default App;
+export default App
