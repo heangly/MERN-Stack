aaab4099705655e7e586cb0f3ad85cbc5cfa0e1f
Heang Ly <heanglypa@gmail.com>
2021-03-21 19:45:01 -0400

    update profile

--- proshop/frontend/src/reducers/userReducers.js
+++ proshop/frontend/src/reducers/userReducers.js
@@ -8,7 +8,10 @@ import {
   USER_LOGOUT,
   USER_REGISTER_FAIL,
   USER_REGISTER_REQUEST,
-  USER_REGISTER_SUCCESS
+  USER_REGISTER_SUCCESS,
+  USER_UPDATE_PROFILE_FAIL,
+  USER_UPDATE_PROFILE_REQUEST,
+  USER_UPDATE_PROFILE_SUCCESS
 } from '../constants/userConstants'
 
 const userLoginReducer = (state = {}, action) => {
@@ -52,4 +55,22 @@ const userDetailsReducer = (state = { user: {} }, action) => {
   }
 }
 
-export { userLoginReducer, userRegisterReducer, userDetailsReducer }
+const userUpdateProfileReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_UPDATE_PROFILE_REQUEST:
+      return { loading: true }
+    case USER_UPDATE_PROFILE_SUCCESS:
+      return { loading: false, success: true, userInfo: action.payload }
+    case USER_UPDATE_PROFILE_FAIL:
+      return { loading: false, error: action.payload }
+    default:
+      return state
+  }
+}
+
+export {
+  userLoginReducer,
+  userRegisterReducer,
+  userDetailsReducer,
+  userUpdateProfileReducer
+}
--- proshop/frontend/src/store.js
+++ proshop/frontend/src/store.js
@@ -10,7 +10,8 @@ import { cartReducer } from './reducers/cartReducers'
 import {
   userDetailsReducer,
   userLoginReducer,
-  userRegisterReducer
+  userRegisterReducer,
+  userUpdateProfileReducer
 } from './reducers/userReducers'
 
 const reducer = combineReducers({
@@ -19,7 +20,8 @@ const reducer = combineReducers({
   cart: cartReducer,
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
-  userDetails: userDetailsReducer
+  userDetails: userDetailsReducer,
+  userUpdateProfile: userUpdateProfileReducer
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
