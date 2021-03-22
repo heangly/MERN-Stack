627da5f14d1642b71b43cbe47dcc1dbc134c3e8c
Heang Ly <heanglypa@gmail.com>
2021-03-22 14:55:07 -0400

    add order screen

--- proshop/frontend/src/screens/PlaceOrderScreen.js
+++ proshop/frontend/src/screens/PlaceOrderScreen.js
@@ -4,9 +4,10 @@ import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import CheckoutSteps from '../components/CheckoutSteps'
 import { Link } from 'react-router-dom'
+import { createOrder } from '../actions/orderActions'
 
-const PlaceOrderScreen = () => {
-  // const dispatch = useDispatch()
+const PlaceOrderScreen = ({ history }) => {
+  const dispatch = useDispatch()
   const cart = useSelector((state) => state.cart)
 
   cart.itemsPrice = cart.cartItems
@@ -23,7 +24,29 @@ const PlaceOrderScreen = () => {
     +cart.taxPrice
   ).toFixed(2)
 
-  const placeOrderHandler = () => {}
+  const orderCreate = useSelector((state) => state.orderCreate)
+  const { order, success, error } = orderCreate
+
+  React.useEffect(() => {
+    if (success) {
+      history.push(`/order/${order._id}`)
+    }
+    // eslint-disable-next-line
+  }, [history, success])
+
+  const placeOrderHandler = () => {
+    dispatch(
+      createOrder({
+        orderItems: cart.cartItems,
+        shippingAddress: cart.shippingAddress,
+        paymentMethod: cart.paymentMethod,
+        itemsPrice: cart.itemsPrice,
+        shippingPrice: cart.shippingPrice,
+        taxPrice: cart.taxPrice,
+        totalPrice: cart.totalPrice
+      })
+    )
+  }
 
   return (
     <>
@@ -114,6 +137,10 @@ const PlaceOrderScreen = () => {
                   <Col>Total</Col>
                   <Col>${cart.totalPrice}</Col>
                 </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                {error && <Message>{error}</Message>}
               </ListGroup.Item>
 
               <ListGroup.Item>
--- proshop/frontend/src/store.js
+++ proshop/frontend/src/store.js
@@ -13,6 +13,7 @@ import {
   userRegisterReducer,
   userUpdateProfileReducer
 } from './reducers/userReducers'
+import { orderCreateReducer } from './reducers/orderReducers'
 
 const reducer = combineReducers({
   productList: productListReducer,
@@ -21,7 +22,8 @@ const reducer = combineReducers({
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
-  userUpdateProfile: userUpdateProfileReducer
+  userUpdateProfile: userUpdateProfileReducer,
+  orderCreate: orderCreateReducer
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
