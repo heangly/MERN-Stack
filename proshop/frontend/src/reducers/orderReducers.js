ae17839903c4b61af4e3686005c5519231b4f144
Heang Ly <heanglypa@gmail.com>
2021-03-23 12:29:00 -0400

    add order screen

--- proshop/frontend/src/reducers/orderReducers.js
+++ proshop/frontend/src/reducers/orderReducers.js
@@ -1,7 +1,10 @@
 import {
   ORDER_CREATE_FAIL,
   ORDER_CREATE_REQUEST,
-  ORDER_CREATE_SUCCESS
+  ORDER_CREATE_SUCCESS,
+  ORDER_DETAILS_FAIL,
+  ORDER_DETAILS_REQUEST,
+  ORDER_DETAILS_SUCCESS
 } from '../constants/orderConstant'
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -21,6 +24,22 @@ export const orderCreateReducer = (state = {}, action) => {
         loading: false,
         error: action.payload
       }
+    default:
+      return state
+  }
+}
+
+export const orderDetailsReducer = (
+  state = { loading: true, orderItems: [], shippingAddress: {} },
+  action
+) => {
+  switch (action.type) {
+    case ORDER_DETAILS_REQUEST:
+      return { loading: true, ...state }
+    case ORDER_DETAILS_SUCCESS:
+      return { loading: false, order: action.payload }
+    case ORDER_DETAILS_FAIL:
+      return { loading: false, error: action.payload }
     default:
       return state
   }
