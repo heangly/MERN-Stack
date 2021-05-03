226e0e7668d9741774784d393c6d5b5cb220cdcc
Heang Ly <heanglypa@gmail.com>
2021-05-03 11:10:57 -0400

    add product detail screen

--- proshop/frontend/src/App.js
+++ proshop/frontend/src/App.js
@@ -1,20 +1,25 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Header from './components/Header'
 import HomeScreen from './screen/HomeScreen'
 import Footer from './components/Footer'
 import { Container } from 'react-bootstrap'
+import ProductScreen from './screen/ProductScreen'
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-5'>
         <Container>
-          <HomeScreen />
+          <Switch>
+            <Route exact path='/' component={HomeScreen} />
+            <Route exact path='/product/:id' component={ProductScreen} />
+          </Switch>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   )
 }
 
