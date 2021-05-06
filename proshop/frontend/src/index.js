b9c68dbbb717efaa0e2e93c5b581d71fd74ec8e3
Heang Ly <heanglypa@gmail.com>
2021-05-06 16:07:34 -0400

    add redux

--- proshop/frontend/src/index.js
+++ proshop/frontend/src/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { Provider } from 'react-redux'
+import store from './stores'
 import './styles/bootstrap.min.css'
 import './styles/index.css'
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+)
--- proshop/frontend/src/screen/HomeScreen.js
+++ proshop/frontend/src/screen/HomeScreen.js
@@ -1,29 +1,35 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { listProducts } from '../actions/productActions'
 import Product from '../components/Product'
 import { Col, Row } from 'react-bootstrap'
-import axios from 'axios'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([])
+  const dispatch = useDispatch()
+  const { loading, error, products } = useSelector((state) => state.productList)
 
   useEffect(() => {
-    const fetchData = async () => {
-      const { data } = await axios.get('/api/products')
-      setProducts(data)
-    }
-    fetchData()
-  }, [])
+    dispatch(listProducts())
+  }, [dispatch])
 
   return (
     <>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message>{error}</Message>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   )
 }
