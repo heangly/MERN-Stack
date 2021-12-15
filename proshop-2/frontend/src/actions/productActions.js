a75e6c77a02ee5e2caad349b71ef3215abd21075
Heang Ly <heanglypa@gmail.com>
2021-12-14 19:45:53 -0500

    redux for product screen

--- proshop-2/frontend/src/actions/productActions.js
+++ proshop-2/frontend/src/actions/productActions.js
@@ -1,7 +1,10 @@
 import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
-  PRODUCT_LIST_FAIL
+  PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL
 } from '../constants/productConstants'
 import axios from 'axios'
 
@@ -28,4 +31,26 @@ const listProducts = () => async (dispatch) => {
   }
 }
 
-export { listProducts }
+const listProductDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({
+      type: PRODUCT_DETAILS_REQUEST
+    })
+    const { data } = await axios.get(`/api/products/${id}`)
+
+    dispatch({
+      type: PRODUCT_DETAILS_SUCCESS,
+      payload: data
+    })
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+    })
+  }
+}
+
+export { listProducts, listProductDetails }
