7fb62ee9a91c376f1dc912e8d3ae12b45722422d
Heang Ly <heanglypa@gmail.com>
2021-12-17 13:15:51 -0500

    register user

--- proshop-2/frontend/src/App.js
+++ proshop-2/frontend/src/App.js
@@ -5,6 +5,8 @@ import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductSreen'
 import CartScreen from './screens/CartScreen'
+import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
 
 const App = () => {
   return (
@@ -12,9 +14,11 @@ const App = () => {
       <Header />
       <main className='py-5'>
         <Container>
-          <Route exact path='/' component={HomeScreen} />
+          <Route path='/login' component={LoginScreen} />
+          <Route path='/register' component={RegisterScreen} />
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
+          <Route exact path='/' component={HomeScreen} />
         </Container>
       </main>
       <Footer />
--- proshop-2/frontend/src/store.js
+++ proshop-2/frontend/src/store.js
@@ -6,17 +6,28 @@ import {
   productListReducer
 } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
+import { userLoginReducer, userRegisterReducer } from './reducers/userReducers'
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetail: productDetailReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  userLogin: userLoginReducer,
+  userRegister: userRegisterReducer
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : []
-const initialState = { cart: { cartItems: cartItemsFromStorage } }
+
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null
+
+const initialState = {
+  cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage }
+}
 const middleware = [thunk]
 
 const store = createStore(
